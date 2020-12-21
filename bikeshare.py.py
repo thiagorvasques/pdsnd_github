7453d95df25fefc5ff51a6c21d48cc1446fb67fc
Thiago Vasques <thiagorvasques@gmail.com>
2020-12-21 12:23:13 +0000

    docs: line comments changed to improve code visualization

--- bikeshare.py.py
+++ bikeshare.py.py
@@ -73,7 +73,7 @@ def load_data(city, month, day):
         
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    #Displays statistics on the most frequent times of travel.
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
@@ -95,7 +95,7 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    #Displays statistics on the most popular stations and trip.
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
@@ -117,7 +117,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    #Displays statistics on the total and average trip duration.
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
@@ -136,7 +136,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    #Displays statistics on bikeshare users
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
